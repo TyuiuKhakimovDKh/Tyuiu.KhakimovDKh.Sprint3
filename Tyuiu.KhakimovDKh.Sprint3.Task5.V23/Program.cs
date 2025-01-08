e1a5efcc13deb575cdf70a214319d0af05041a3f
Khakimov Doston <Khakimovmma12@gmail.com>
2025-01-09 00:44:12 +0500

    Task5

---
+++ Tyuiu.KhakimovDKh.Sprint3.Task5.V23/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+using Tyuiu.KhakimovDKh.Sprint3.Task5.V23.Lib;
+
+
+
+namespace Tyuiu.KhakimovDKh.Sprint3.Task5.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Хакимов Д.Х. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Хакимов Достонбек Хисравович | ИИПб-24-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                         Вычислить значение функции при x = 2             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 10;
+            Console.WriteLine("Переменная Х = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue2);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            Console.ReadKey();
+        }
+    }
+}
